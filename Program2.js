4bca2a3c0f0773da4a2d8da3de3cc6aba98394ee
nantiya-coe1 <b231163@gmail.com>
2022-01-10 22:43:27 +0700

    first commit

---
+++ Program2.js
@@ -0,0 +1,20 @@
+// โปรแกรมคำนวณเกรดเฉลี่ย 
+// นางสาวนันทิยา บุตรมาตา 6135512010
+
+let score = 63;
+
+if ((score >= 80) && (score <= 100)) {
+    console.log("You grade is A");
+}
+else if ((score >= 70) && (score <= 79)) {
+    console.log("You grade is B");
+}
+else if ((score >= 60) && (score <= 69)) {
+    console.log("You grade is C");
+}
+else if ((score >= 50) && (score <= 59)) {
+    console.log("You grade is D");
+}
+else {
+    console.log("You grade is E");
+}
---
+++ Program3.js
@@ -0,0 +1,10 @@
+// โปรแกรมบวก ลบ คูณ หารเลข
+// นางสาวนันทิยา บุตรมาตา 6135512010
+
+let a = 45;
+let b = 5;
+
+console.log('a + b = ' + (a + b));
+console.log('a - b = ' + (a - b));
+console.log('a * b = ' + (a * b)); 
+console.log('a / b = ' + (a / b)); 
